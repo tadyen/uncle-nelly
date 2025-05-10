1b43613a40742d90c005ab1919830d90b96684ba
tadyen <tadyen.dev@gmail.com>
2025-05-10 20:35:53 +1000

    Cooking sim uses react now

--- web/src/SubPages/CookingSim.tsx
+++ web/src/SubPages/CookingSim.tsx
@@ -1,20 +1,34 @@
-function Steps(){
+import React from 'react';
+
+
+type StepProps = {
+  progress: number;
+}
+function Steps(props: StepProps){
+  //progress starts at 1
+  const progress = props.progress;
   return (
     <div className="mx-auto">
       <ul className="steps">
-        <li className="step step-primary">Select Base</li>
-        <li className="step step-primary">Select Ingredients</li>
-        <li className="step">Crackulate</li>
-        <li className="step">Result</li>
+        <li className={(progress>=1) ? "step step-primary" : "step"}>Select Base</li>
+        <li className={(progress>=2) ? "step step-primary" : "step"}>Select Ingredients</li>
+        <li className={(progress>=3) ? "step step-primary" : "step"}>Crack-ulate</li>
+        <li className={(progress>=4) ? "step step-primary" : "step"}>Result</li>
       </ul>
     </div>
   )
 }
 
 export default function CookingSim(){
-    return(<>
-      <div className="mx-auto flex">
-        <Steps/>
-      </div>
-    </>)
+  const [progress, setProgress] = React.useState<number>(1);
+
+  React.useEffect(() => {
+    setProgress(3);
+  },[]);
+
+  return(<>
+    <div className="mx-auto flex">
+      <Steps {...{ progress }} />
+    </div>
+  </>)
 }
